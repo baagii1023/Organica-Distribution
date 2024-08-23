afb2356e4030deb8a025b8bca0d5aa4388625f87
Baagii <baagii1993@yahoo.com>
2024-08-23 15:06:12 -0700

    uploaded green logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Navigation from '../components/Navigation';
 import Swiper from '../components/Swiper';
+import data from '../data/translation.json'; 
 
 export default function Home () {
-	return (
-		<div>
-			<Navigation/>
-			<Swiper/>
-			<h1 className='bg-blue-400'>penis</h1>
-		</div>
-	)
-}
+    console.log(data); // Check if the data is correctly logged
 
+    return (
+        <div>
+            <Navigation />
+            <Swiper />
+            <h1 className='bg-blue-400'>penis</h1>
+            <div style={{ width: '100px', height: '100px' }}>
+                <div dangerouslySetInnerHTML={{ __html: data.en.whyUsSvg4[0] }} />
+            </div>
+        </div>
+    );
+}
