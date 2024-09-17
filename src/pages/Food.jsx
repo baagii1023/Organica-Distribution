214476f78aac372d615c7bdea6634d5184fcd628
Anand <cell.unobtainable.88@gmail.com>
2024-09-17 12:17:20 -0700

    fixed the food image json issue, 50% done with Food Page <3

--- src/pages/Food.jsx
+++ src/pages/Food.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import Navigation from '../components/Navigation';
+import food from '/food.png'
+import foodExtra from '/food-extra.jpeg'
 
+import { useContext } from 'react'
+import productData from '../data/translation-products.json';
+import { LanguageContext } from '../context/LanguageContext'; // Import LanguageContext
+import { Link } from 'react-router-dom';
 const Food = () => {
+	const { language } = useContext(LanguageContext); // Use LanguageContext
+	const productText = productData[language] || {};
 	return (
-		<div>Food
+		<div>
 			<Navigation />
+			<div className="relative flex items-center justify-center">
+				<img src={food} alt="" className="w-screen object-cover h-[400px]" />
+				<div className='absolute z-10 left-1/4'>
+					<h1 className="text-white text-4xl my-4">SWEET'S <br /> FRUIT & GREEN TEA</h1>
+					<a href="/" className='bg-orange-500 p-2'>Shop Products</a>
+				</div>
+			</div>
+			<div className='w-full flex flex-col items-center py-[100px]'>
+				<h1 className='font-[800] text-[48px]'>Food</h1>
+				<div className='pt-[62px] pb-[158px]'>
+					{productText.Food.map((product, index) => (
+						<Link key={index} to={`/details/${product.FoodName}`}>
+							<div className="flex flex-col items-center w-[270px] mx-auto border border-black">
+								<img src={product.image} alt={product.Heading} className="h-full w-full" />
+								<h3>{product.FoodName}</h3>
+								<p>{product.SubHeading}</p>
+							</div>
+						</Link>
+					))}
+				</div>
+				<div className='mx-auto w-[1200px] flex border border-[#C7BFBA] h-[440px]'>
+					<div className='basis-1/2' style={{ backgroundImage: foodExtra, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+
+					</div>
+
+					<div className='basis-1/2 flex flex-col items-center justify-center'>
+						<div className='text-start'>
+							<h2>WHOLESALE FOOD</h2>
+							<p>The beverages at Wise Trading Group is a best-selling collection, reflecting our dedication and commitment to being premier wholesale drinks suppliers. This category is designed to cater to a diverse assortment of tastes and preferences, making it a prime wholesale destination for a wide range of beverages.</p>
+
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
